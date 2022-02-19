a92eb17c5453643ad38028dba0f8c24a5a877500
noobandnothing <popcorn.server.project@gmail.com>
2022-02-19 22:31:28 +0200

    Arduino : Circuit Code

---
+++ merged_proj_UNO/merged_proj_UNO.ino
@@ -0,0 +1,105 @@
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 5 //define green LED pin
+#define LED_R 4 //define red LED
+#define RELAY 3 //relay pin
+#define BUZZER 2 //buzzer pin
+#define ACCESS_DELAY 2000
+#define DENIED_DELAY 1000
+#define RX 7 // RX(connect TX of esp8266)
+#define TX 6 // TX(connect RX of esp8266)
+SoftwareSerial mySerial(RX, TX);
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  mySerial.begin(9600);
+  SPI.begin();          // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  //digitalWrite(RELAY, HIGH);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+}
+void loop() 
+{
+  R_F_E();
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "53 DD 06 39") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(RELAY, HIGH);
+    digitalWrite(LED_G, HIGH);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY, LOW);
+    digitalWrite(LED_G, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(DENIED_DELAY);
+    digitalWrite(LED_R, LOW);
+    noTone(BUZZER);
+  }
+}
+
+// Recieve Message in serial 960 (UNO)
+void R_F_E(){
+   String IncomingString="";
+ boolean StringReady = false;
+ 
+ while (mySerial.available()){
+   IncomingString=mySerial.readString();
+   StringReady= true;
+  }
+ 
+  if (StringReady){
+    Serial.println("Received String: " + IncomingString);
+  if (IncomingString.indexOf("1560QW") >= 0){
+        digitalWrite(3, HIGH);
+        digitalWrite(LED_G, HIGH);
+        delay(ACCESS_DELAY);
+        digitalWrite(LED_G, LOW);
+        
+  }else if (IncomingString.indexOf("1525QW") >= 0){
+        digitalWrite(3, LOW);
+  }
+  }
+  
+}
